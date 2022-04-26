cb3a501735823615a186b5e53b53fa010f32cd65
Yurii Samoienko <yurii.samoienko@gmail.com>
2022-04-26 16:40:32 +0300

    added Mac App support

---
+++ SwiftUiExtensionMac/SwiftUiExtensionMac.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUiExtensionMac.h
+//  SwiftUiExtensionMac
+//
+//  Created by Yurii Samoienko on 26.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUiExtensionMac.
+FOUNDATION_EXPORT double SwiftUiExtensionMacVersionNumber;
+
+//! Project version string for SwiftUiExtensionMac.
+FOUNDATION_EXPORT const unsigned char SwiftUiExtensionMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUiExtensionMac/PublicHeader.h>
+
+
